ae6cdd64405873d6ec51ffe0563a2889919c49fc
Allan Feller <16980156+AFeller9999@users.noreply.github.com>
2018-10-04 23:33:45 -0600

    Update Robot.java

--- Echo/src/org/usfirst/frc/team3045/robot/Robot.java
+++ Echo/src/org/usfirst/frc/team3045/robot/Robot.java
@@ -275,48 +275,48 @@ public class Robot extends IterativeRobot {
 			talon4.set(ControlMode.PercentOutput, 1);
 		} else if (dpadInput == 90) {
 			// Bottom right and top left wheel go full
-			talon1.set(ControlMode.PercentOutput, -1);
+			talon1.set(ControlMode.PercentOutput, 1);
 			talon0.set(ControlMode.PercentOutput, 1);
-			talon5.set(ControlMode.PercentOutput, 1);
+			talon5.set(ControlMode.PercentOutput, -1);
 			talon4.set(ControlMode.PercentOutput, -1);
 		} else if (dpadInput == 270) {
 			// Bottom right and top left wheel go full
-			talon1.set(ControlMode.PercentOutput, 1);
+			talon1.set(ControlMode.PercentOutput, -1);
 			talon0.set(ControlMode.PercentOutput, -1);
-			talon5.set(ControlMode.PercentOutput, -1);
+			talon5.set(ControlMode.PercentOutput, 1);
 			talon4.set(ControlMode.PercentOutput, 1);
 		} else if (dpadInput == 45) {
 			// Bottom right and top left wheel go full, rest 0
 			talon1.set(ControlMode.PercentOutput, 0);
-			talon0.set(ControlMode.PercentOutput, 1);
+			talon0.set(ControlMode.PercentOutput, -1);
 			talon5.set(ControlMode.PercentOutput, 1);
 			talon4.set(ControlMode.PercentOutput, 0);
 		} else if (dpadInput == 315) {
 			// Bottom left and top right wheel go full, rest 0
-			talon1.set(ControlMode.PercentOutput, 1);
+			talon1.set(ControlMode.PercentOutput, -1);
 			talon0.set(ControlMode.PercentOutput, 0);
 			talon5.set(ControlMode.PercentOutput, 0);
 			talon4.set(ControlMode.PercentOutput, 1);
 		} else if (dpadInput == 225) {
 			// Bottom right and top left wheel go full -, rest 0
 			talon1.set(ControlMode.PercentOutput, 0);
-			talon0.set(ControlMode.PercentOutput, -1);
+			talon0.set(ControlMode.PercentOutput, 1);
 			talon5.set(ControlMode.PercentOutput, -1);
 			talon4.set(ControlMode.PercentOutput, 0);
 		} else if (dpadInput == 135) {
 			// Bottom left and top right wheel go full -, rest 0
-			talon1.set(ControlMode.PercentOutput, -1);
+			talon1.set(ControlMode.PercentOutput, 1);
 			talon0.set(ControlMode.PercentOutput, 0);
 			talon5.set(ControlMode.PercentOutput, 0);
 			talon4.set(ControlMode.PercentOutput, -1);
 		} else if (lTrigger > 0) { // Turning
-			talon1.set(ControlMode.PercentOutput, lTrigger);
-			talon0.set(ControlMode.PercentOutput, lTrigger);
+			talon1.set(ControlMode.PercentOutput, -lTrigger);
+			talon0.set(ControlMode.PercentOutput, -lTrigger);
 			talon5.set(ControlMode.PercentOutput, -lTrigger);
 			talon4.set(ControlMode.PercentOutput, -lTrigger);
 		} else if (rTrigger > 0) {
-			talon1.set(ControlMode.PercentOutput, -rTrigger);
-			talon0.set(ControlMode.PercentOutput, -rTrigger);
+			talon1.set(ControlMode.PercentOutput, rTrigger);
+			talon0.set(ControlMode.PercentOutput, rTrigger);
 			talon5.set(ControlMode.PercentOutput, rTrigger);
 			talon4.set(ControlMode.PercentOutput, rTrigger);
 		} else {
